5473205d5ed7df3543b83276b2b2f940fe340e5b
sinnermann <ealijev@gmail.com>
2024-12-04 20:29:58 +0400

    last commit

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,6 +1,6 @@
 import loginImg from "../../assets/login.svg";
 
-const Login = ({onRegister, onReset}) => {
+const Login = ({ onRegister, onReset }) => {
   return (
     <div className="main-container --flex-center">
       <div className="img-container">
@@ -10,10 +10,21 @@ const Login = ({onRegister, onReset}) => {
         <form className="--form-control">
           <h2 className="--color-danger --text">Login</h2>
           <input type="text" className="--width-100" placeholder="Username" />
-          <input type="password" className="--width-100" placeholder="Password" />
+          <input
+            type="password"
+            className="--width-100"
+            placeholder="Password"
+          />
           <button className="--btn --btn-primary --btn-block">Login</button>
-          <a href="#" className="--text-sm" onClick={onReset}>Forgot Password</a>
-          <span className="--text-sm --block">Don't have an account? <a href="#" className="--text-sm" onClick={onRegister}>Register</a></span>
+          <a href="#" className="--text-sm" onClick={onReset}>
+            Forgot Password
+          </a>
+          <span className="--text-sm --block">
+            Don't have an account?{" "}
+            <a href="#" className="--text-sm" onClick={onRegister}>
+              Register Now
+            </a>
+          </span>
         </form>
       </div>
     </div>
